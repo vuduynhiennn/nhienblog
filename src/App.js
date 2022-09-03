cb7a72f9806fbae929ee0aac7c0d04c6178246b9
vuduynhiennn <vuduynhiencute@gmail.com>
2022-09-04 00:16:17 +0700

    main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Footer from "./components/Footer"
 import Header from "./components/Header"
+import Main from "./components/Main"
 
 function App() {
     return (
         <>
             <Header />
+            <Main />
             <Footer />
         </>
     )
---
+++ src/components/Main/index.js
@@ -0,0 +1,105 @@
+import styles from './Main.module.scss';
+
+const Main = () => {
+    return (
+        <div className={styles.postCtn}>
+
+            <div className={styles.postItem}>
+                <div className={styles.postImgCtn}> </div>
+
+                <h2 className={styles.postTitle}> đây là tiêu đề cho bài viết bạn ạ bạn test xem như này có ổn không hả bạn ?</h2>
+            </div>
+
+
+            <div className={styles.postItem}>
+                <div className={styles.postImgCtn}> </div>
+
+                <h2 className={styles.postTitle}> đây là tiêu đề cho bài viết bạn ạ bạn test xem như này có ổn không hả bạn ?</h2>
+            </div>
+
+
+            <div className={styles.postItem}>
+                <div className={styles.postImgCtn}> </div>
+
+                <h2 className={styles.postTitle}> đây là tiêu đề cho bài viết bạn ạ bạn test xem như này có ổn không hả bạn ?</h2>
+            </div>
+
+
+            <div className={styles.postItem}>
+                <div className={styles.postImgCtn}> </div>
+
+                <h2 className={styles.postTitle}> đây là tiêu đề cho bài viết bạn ạ bạn test xem như này có ổn không hả bạn ?</h2>
+            </div>
+
+            <div className={styles.postItem}>
+                <div className={styles.postImgCtn}> </div>
+
+                <h2 className={styles.postTitle}> đây là tiêu đề cho bài viết bạn ạ bạn test xem như này có ổn không hả bạn ?</h2>
+            </div>
+
+
+            <div className={styles.postItem}>
+                <div className={styles.postImgCtn}> </div>
+
+                <h2 className={styles.postTitle}> đây là tiêu đề cho bài viết bạn ạ bạn test xem như này có ổn không hả bạn ?</h2>
+            </div>
+
+
+            <div className={styles.postItem}>
+                <div className={styles.postImgCtn}> </div>
+
+                <h2 className={styles.postTitle}> đây là tiêu đề cho bài viết bạn ạ bạn test xem như này có ổn không hả bạn ?</h2>
+            </div>
+
+
+            <div className={styles.postItem}>
+                <div className={styles.postImgCtn}> </div>
+
+                <h2 className={styles.postTitle}> đây là tiêu đề cho bài viết bạn ạ bạn test xem như này có ổn không hả bạn ?</h2>
+            </div>
+
+            <div className={styles.postItem}>
+                <div className={styles.postImgCtn}> </div>
+
+                <h2 className={styles.postTitle}> đây là tiêu đề cho bài viết bạn ạ bạn test xem như này có ổn không hả bạn ?</h2>
+            </div>
+
+
+            <div className={styles.postItem}>
+                <div className={styles.postImgCtn}> </div>
+
+                <h2 className={styles.postTitle}> đây là tiêu đề cho bài viết bạn ạ bạn test xem như này có ổn không hả bạn ?</h2>
+            </div>
+
+
+            <div className={styles.postItem}>
+                <div className={styles.postImgCtn}> </div>
+
+                <h2 className={styles.postTitle}> đây là tiêu đề cho bài viết bạn ạ bạn test xem như này có ổn không hả bạn ?</h2>
+            </div>
+
+
+            <div className={styles.postItem}>
+                <div className={styles.postImgCtn}> </div>
+
+                <h2 className={styles.postTitle}> đây là tiêu đề cho bài viết bạn ạ bạn test xem như này có ổn không hả bạn ?</h2>
+            </div>
+
+            <div className={styles.postItem}>
+                <div className={styles.postImgCtn}> </div>
+
+                <h2 className={styles.postTitle}> đây là tiêu đề cho bài viết bạn ạ bạn test xem như này có ổn không hả bạn ?</h2>
+            </div>
+
+            <div className={styles.postItem}>
+                <div className={styles.postImgCtn}> </div>
+
+                <h2 className={styles.postTitle}> đây là tiêu đề cho bài viết bạn ạ bạn test xem như này có ổn không hả bạn ?</h2>
+            </div>
+
+         
+        </div> 
+    )
+}
+
+export default Main;
