c9c94aa0c3d875f2abc2ea317bbaa928a46dad6b
vuduynhiennn <vuduynhiencute@gmail.com>
2022-09-03 18:47:18 +0700

    delete and work again

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
-import GlobalStyle from "./components/GlobalStyle/GlobalStyle"
-import Header from "./components/Header/Header"
-import Main from "./components/Main/Main"
-
-
 function App() {
     return (
-        <GlobalStyle>
-            <Header />
-            <Main />
-        </GlobalStyle>
+        <>
+            Coming soon...
+        </>
     )
 }
 
--- src/components/GlobalStyle/GlobalStyle.js
+++
@@ -1,7 +0,0 @@
-import "./GlobalStyle.scss"
-
-let GlobalStyle = ({ children }) => {
-    return children
-}
-
-export default GlobalStyle
