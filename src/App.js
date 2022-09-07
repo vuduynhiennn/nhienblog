8074d33c11acce690adde86fb31ca92035732e53
vuduynhiennn <vuduynhiencute@gmail.com>
2022-09-08 00:18:15 +0700

    djt me bug

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import Apost from "./components/Apost"
+// import Apost from "./components/Apost"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
-// import Main from "./components/Main"
+import Main from "./components/Main"
 
 function App() {
     return (
         <>
             <Header />
-            {/* <Main /> */}
-            <Apost />
+            <Main />
+            {/* <Apost /> */}
             <Footer />
         </>
     )
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,35 @@
 import styles from './Header.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
+
+        const categoriesMenu = document.getElementById('cateMenu');
+
+        const handleAppearMenu = () => {
+            categoriesMenu.style.display = 'block';
+        }
+
     return (
         <>
             <div className={styles.header}>
-                <h1> header... </h1>
-                <h1> comming soon... </h1>
+
+                <div className={styles.categoriesMenu} onClick = { handleAppearMenu }> 
+                    <FontAwesomeIcon icon={faBars} />
+                </div>
+
+                <ul className={styles.categories} id='cateMenu'>
+                    <li>
+                        <a href='/'>
+                            javascipt
+                        </a>
+                    </li>
+                    <li>html, css</li>
+                    <li>reactjs</li>
+                    <li>react native</li>
+                    <li>express js</li>
+                    <li>django</li>
+                </ul>
             </div>
         </>
     )
