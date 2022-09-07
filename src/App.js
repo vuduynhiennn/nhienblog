7c95c0dc553e4eb51654830cf24dc2482458209c
vuduynhiennn <vuduynhiencute@gmail.com>
2022-09-08 00:30:04 +0700

    removed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-// import Apost from "./components/Apost"
-import Footer from "./components/Footer"
-import Header from "./components/Header"
-import Main from "./components/Main"
 
 function App() {
     return (
         <>
-            <Header />
-            <Main />
-            {/* <Apost /> */}
-            <Footer />
+            <h1> Comming soon...</h1>
         </>
     )
 }
